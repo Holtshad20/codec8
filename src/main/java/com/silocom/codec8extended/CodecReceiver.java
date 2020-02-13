d3b186bece15402c0383ee784fa1de82e500f038
silocom01 <silocom01@silocom01-PC>
2020-02-13 13:24:42 -0400

    Codec8 Extended for FMB920 devices

---
+++ src/main/java/com/silocom/codec8extended/CodecReceiver.java
@@ -0,0 +1,13 @@
+/*
+ * @Author Fernando Gonzalez.
+ */
+package com.silocom.codec8extended;
+
+/**
+ *
+ * @author silocom01
+ */
+public class CodecReceiver {
+   
+    
+}
---
+++ src/main/java/com/silocom/codec8extended/Main.java
@@ -0,0 +1,21 @@
+/*
+ * @Author Fernando Gonzalez.
+ */
+package com.silocom.codec8extended;
+
+import com.silocom.m2m.layer.physical.PhysicalLayer;
+
+/**
+ *
+ * @author silocom01
+ */
+public class Main {
+
+    public static void main(String[] args) {
+       String payload = "08010000016B40D8EA30010000000000000000000000000000000105021503010101425E0F01F10000601A014E000000000000000001";
+       String IpSrc = "192.168.2.40"; 
+       com.silocom.m2m.layer.physical.Connection con = PhysicalLayer.addConnection(1, 17500, IpSrc);  //colocar tipo de conexion e IP valido
+       
+       System.out.println(" CRC16: 0x0000" + CRC16.calcCRC16(payload));
+    }
+}
